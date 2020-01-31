1d46cd47d8664f11c097568c1c58151e6f6a50fc
davidmbedford <david.m.bedford1@gmail.com>
2020-01-31 15:47:08 -0500

    imports first established and working in appjs

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './pages/home/index.js';
+import Portfolio from './pages/portfolio/index.js';
+import Skills from './pages/skills/index.js';
+import Contact from './pages/contact/index.js';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Home/>
+    <Portfolio/>
+    <Skills/>
+    <Contact/>
+    </>
   );
 }
 
