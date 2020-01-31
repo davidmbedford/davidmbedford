36972a7b5c4b54cc4d7c3d0279df44215551a95e
davidmbedford <david.m.bedford1@gmail.com>
2020-01-31 16:49:56 -0500

    set up more framework code for components and appjs - also installed proper router imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,15 +7,20 @@ import Home from './pages/home/index.js';
 import Portfolio from './pages/portfolio/index.js';
 import Skills from './pages/skills/index.js';
 import Contact from './pages/contact/index.js';
-
+import Navigation from './components/navigation/index.js';
 
 function App() {
   return (
     <>
-    <Home/>
-    <Portfolio/>
-    <Skills/>
-    <Contact/>
+    <Router>
+    <Navigation /> 
+      <Switch>
+        <Home/>
+        <Portfolio/>
+        <Skills/>
+        <Contact/>
+      </Switch>
+    </Router>
     </>
   );
 }
---
+++ src/components/navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Navigation() {
+    return(
+        <div className="col s12 l3">
+            <h1>This is the Navigation zone</h1>
+        </div>
+    )
+}
+
+export default Navigation;
