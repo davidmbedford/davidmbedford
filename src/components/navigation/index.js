a271ead12035587bdf07a4787c11930de7331539
davidmbedford <david.m.bedford1@gmail.com>
2020-02-13 14:11:11 -0500

    navbar in proper column setup and functions on resize - cosmetically awful, but not a priority atm

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,48 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import './style.css';
 
-function Navigation() {
+class Navigation extends Component {
+    componentDidMount() {
+        const M = window.M;
+        document.addEventListener('DOMContentLoaded', function() {
+          var elems = document.querySelectorAll('.sidenav');
+          var instances = M.Sidenav.init(elems, {});
+        });
+      }
+
+    render () {
     return(
         <>
-        <nav id="transparent">
-        <div className="nav-wrapper">
-          <a href="https://www.linkedin.com/in/david-bedford-027b99190/" className="brand-logo">dmb</a>
-          <a data-target="mobile-demo" className="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
+        <div className="col s12 l3">
+        <nav>
+            <div className="nav-wrapper">
+                <a href="https://www.linkedin.com/in/david-bedford-027b99190/" className="brand-logo">dmb</a>
+                <a data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                <div className="container">
+                    <ul class="right hide-on-med-and-down">
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/portfolio'>Portfolio</Link></li>
+                        <li><Link to='/skills'>Skills</Link></li>
+                        <li><Link to='/contact'>Contact</Link></li>
+                    </ul>
+                </div> 
+            </div>
+        </nav>
+
+        <ul className="sidenav" id="slide-out">
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/portfolio'>Portfolio</Link></li>
             <li><Link to='/skills'>Skills</Link></li>
             <li><Link to='/contact'>Contact</Link></li>
-          </ul>
-        </div>
-      </nav>
-
-      <ul className="sidenav">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/portfolio'>Portfolio</Link></li>
-        <li><Link to='/skills'>Skills</Link></li>
-        <li><Link to='/contact'>Contact</Link></li>
-      </ul>
+        </ul>
         
-        <div className="col s12 l3">
-            <h1>This is the Navigation zone</h1>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/portfolio'>Portfolio</Link></li>
-                <li><Link to='/skills'>Skills</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-            <ul id="slide-out" className="sidenav">
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/portfolio'>Portfolio</Link></li>
-                <li><Link to='/skills'>Skills</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-        </div>
+</div>
 
-</>
+        </>
     )
+}
 }
 
 export default Navigation;
