9ca4642a0ee7f313eb4f44a5ef5e0f6ef6c58498
davidmbedford <david.m.bedford1@gmail.com>
2020-02-13 14:52:20 -0500

    most basic setup - all links work, footer included

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,9 +7,11 @@ function Footer() {
       <div className="footer-copyright" id="footerbottom">
         <div className="container">
           this site designed by <span><a href="https://davidmbedford.github.io">davidmbedford</a></span>
-          <a className="grey-text right" href="https://www.instagram.com/littlunmusic/?hl=en"><i className="fab fa-instagram fa-lg"></i></a>
-          <a className="grey-text right" href="https://twitter.com/littlunmusic"><i className="fab fa-twitter fa-lg"></i></a>
-          <a className="grey-text right" href="https://www.facebook.com/littlunmusic/"><i className="fab fa-facebook fa-lg"></i></a>
+          <a className="grey-text right" href="https://docs.google.com/document/d/1ozBWzyouy6FgoyrDW90FbzAmbWb-rNfD70IWsBV85-M/edit?usp=sharing">
+            <i class="far fa-file"></i>
+              </a>
+          <a className="grey-text right" href="https://github.com/davidmbedford"><i class="fab fa-github"></i></a>
+          <a className="grey-text right" href="https://www.linkedin.com/in/davidmbedford/"><i class="fab fa-linkedin"></i></a>
         </div>
       </div>
     </footer>
