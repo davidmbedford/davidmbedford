cc4f289069a6587749cb56509f1a2c279a15fadf
davidmbedford <david.m.bedford1@gmail.com>
2020-02-13 13:49:50 -0500

    inserted traditional navbar, got it working - will use as case-tester for main nav zone

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -4,6 +4,27 @@ import './style.css';
 
 function Navigation() {
     return(
+        <>
+        <nav id="transparent">
+        <div className="nav-wrapper">
+          <a href="https://www.linkedin.com/in/david-bedford-027b99190/" className="brand-logo">dmb</a>
+          <a data-target="mobile-demo" className="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/portfolio'>Portfolio</Link></li>
+            <li><Link to='/skills'>Skills</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul className="sidenav">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/portfolio'>Portfolio</Link></li>
+        <li><Link to='/skills'>Skills</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+      </ul>
+        
         <div className="col s12 l3">
             <h1>This is the Navigation zone</h1>
             <ul>
@@ -19,6 +40,8 @@ function Navigation() {
                 <li><Link to='/contact'>Contact</Link></li>
             </ul>
         </div>
+
+</>
     )
 }
 
