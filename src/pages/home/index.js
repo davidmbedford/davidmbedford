584ba08a910a7b98aaa2f096df08b6811e4b1652
davidmbedford <david.m.bedford1@gmail.com>
2020-02-22 20:18:45 -0500

    Home page is bare minimum mvp

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,8 +6,11 @@ function Home() {
         <div className="col s12 l9 no-pad">
         <div className="App">
             <div className="App-header">
-                <div className="container">
-                    <h1>Welcome Home</h1>
+                <div className="container left-align">
+                    <h3>Hi!</h3>
+                    <h3>I'm a Full-Stack Developer with a current focus on React and UI/UX design</h3>
+                    <h5>But I'm familiar with the full design process! Check out my work</h5>
+                    <h5>I'm also a project manager, musician, community activist, and avid learner</h5>
                 </div>        
             </div>
         </div>
