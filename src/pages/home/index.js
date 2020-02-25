ee218d24db925bd92e1a3d90e05b37a35707e4ce
davidmbedford <david.m.bedford1@gmail.com>
2020-02-24 19:35:08 -0500

    added route to Hi! and adjusted a lot of colors throughout site

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,9 +8,10 @@ function Home() {
         <div className="App">
             <div className="App-header">
                 <div className="container left-align">
-                    <h3>Hi!</h3>
-                    <h3>I'm a Full-Stack Developer with a current focus on React and UI/UX design</h3>
-                    <h5>But I'm familiar with the full design process! Check out my work</h5>
+                    <h3 id="color1"><a href="/contact">Hi!</a></h3>
+                    <h3>I'm a <span id="color1">Full-Stack Web Developer</span> with a focus on React and UI/UX design</h3>
+                    <br/>
+                    <h5>I'm familiar with the full design process! Check out my <span id="color2">work</span> </h5>
                     <h5>I'm also a project manager, musician, community activist, and avid learner</h5>
                 </div>    
             <div className="container">    
