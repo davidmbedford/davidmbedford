42eda53ae098cd8609318d8b4a3f466147a771f4
davidmbedford <david.m.bedford1@gmail.com>
2020-02-25 09:38:24 -0500

    skills page made to MVP status with new design

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,11 +6,11 @@ function Footer() {
     <footer >
         <div className="container center">
           <div className="row">
-          <a className="grey-text" href="https://docs.google.com/document/d/16ADcaKgLQvL9U5Qq8-KJYXubDxbyXjrrusFbPDU3tuE/edit?usp=sharing">
-            <i class="far fa-file" title="resume"></i>
+          <a className="footerIcon" href="https://docs.google.com/document/d/16ADcaKgLQvL9U5Qq8-KJYXubDxbyXjrrusFbPDU3tuE/edit?usp=sharing">
+            <i className="far fa-file" title="resume"></i>
               </a>
-          <a className="grey-text" href="https://github.com/davidmbedford"><i class="fab fa-github" title="github"></i></a>
-          <a className="grey-text" href="https://www.linkedin.com/in/davidmbedford/"><i class="fab fa-linkedin" title="linkedin"></i></a>
+          <a className="footerIcon" href="https://github.com/davidmbedford"><i className="fab fa-github" title="github"></i></a>
+          <a className="footerIcon" href="https://www.linkedin.com/in/davidmbedford/"><i className="fab fa-linkedin" title="linkedin"></i></a>
         </div>
         </div>
     </footer>
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -35,11 +35,11 @@ function Portfolio() {
                       <span class="bard-title">ZigZag</span>
                     </div>
                     <div className="bard-content">
-                      <p>Are you looking for a new career path? ZigZag contains a personality survey to help
+                      <p>Find your new career today! ZigZag contains a personality survey to help
                         determine the best course for you! We also have a vast database
-                          of related academic programs.</p>
+                        of related academic programs.</p>
                       <br />
-                      <p>Main Tech: React, ES6, node.js, Sql, Express, APIs, HTML/CSS (Materialize)</p>
+                      <p><span className="bold">Main Tech: </span> React, ES6, node.js, Sql, Express, APIs, HTML/CSS (Materialize)</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/asmith7531/team1project3" target="_blank">Code</a>
@@ -57,7 +57,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>Test you connection to the force and click on all of the characters without repeating! This app was built to demonstrate dynamic changes with React.JS</p>
                       <br />
-                      <p>Main Tech: React, ES6, HTML/CSS (BootStrap)</p>
+                      <p><span className="bold">Main Tech:</span> React, ES6, HTML/CSS (BootStrap)</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/davidmbedford/clickergame" target="_blank">Code</a>
@@ -78,7 +78,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>Enables users to find fun events near them! You can search for concerts, fests and more in your area. Find tickets, and directions via our app, and create a profile to save information.</p>
                       <br />
-                      <p>Main Tech: JavaScript, jQuery, various APIs, Firebase, HTML/CSS (Bootstrap)</p>
+                      <p><span className="bold">Main Tech:</span> JavaScript, jQuery, various APIs, Firebase, HTML/CSS (Bootstrap)</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/Parker-Tech/project_one_boiler" target="_blank">Code</a>
@@ -96,7 +96,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>An all-in-one productivity app: Blitz enables you to start your day with a bang, Choose between various widgets, such as traffic reports, weather, and the news, to maximize your morning!</p>
                       <br />
-                      <p>Main Tech: JavaScript, node.js, jQuery, Sql, Handlebars, Express, various APIs, HTML/CSS (Bootstrap)</p>
+                      <p><span className="bold">Main Tech:</span> JavaScript, node.js, jQuery, Sql, Handlebars, Express, various APIs, HTML/CSS (Bootstrap)</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/rachaelbelle/blitzkreig" target="_blank">Code</a>
@@ -118,7 +118,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>A digital storefront designed for your command line interface! There is a client, and manager-facing interface for this app. Customers may view inventory and purchase items; managers can review, stock and add new items.</p>
                       <br />
-                      <p>Main Tech: node.js, jQuery, Inquirer, bash</p>
+                      <p><span className="bold">Main Tech:</span> node.js, jQuery, Inquirer, bash</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/davidmbedford/bamazon-unit12" target="_blank">Code</a>
@@ -136,7 +136,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>This utilizes Firebase to create a live-updating schedule application. This current version is geared towards Train conductors, but the application can be refactored for anyone in need of coordinating a steady routine.</p>
                       <br />
-                      <p>Tech: Firebase, JS, jQuery, HTML/CSS (Bootstrap)</p>
+                      <p><span className="bold">Main Tech:</span> Firebase, JS, jQuery, HTML/CSS (Bootstrap)</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/davidmbedford/LiveSchedule" target="_blank">Code</a>
@@ -157,7 +157,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>This app allows users to take a survey and find like-minded people in their area to hang out with! This app is meant to demonstrate MySql API-routing and Handlebars via node.</p>
                       <br />
-                      <p>Main Tech: JS, jQuery, MySql, Handlebars, HTML/CSS (Materialize)</p>
+                      <p><span className="bold">Main Tech:</span> JS, jQuery, MySql, Handlebars, HTML/CSS (Materialize)</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/davidmbedford/FriendFinder" target="_blank">Code</a>
@@ -175,7 +175,7 @@ function Portfolio() {
                     <div className="bard-content">
                       <p>A jQuery-based Gif engine that utilizes the Giphy API. Users can add buttons to the header and use them to search for gifs of any sort based on what's trending.</p>
                       <br />
-                      <p>Tech: JavaScript, jQuery, HTML, CSS</p>
+                      <p><span className="bold">Main Tech:</span> JavaScript, jQuery, HTML, CSS</p>
                     </div>
                     <div className="bard-action">
                       <a href="https://github.com/davidmbedford/Unit-6-GifTastic" target="_blank">Code</a>
