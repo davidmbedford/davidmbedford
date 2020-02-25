bb458dd7b9216358629b73344b4db8de7a109639
davidmbedford <david.m.bedford1@gmail.com>
2020-02-25 00:23:50 -0500

    footer animated

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,11 +6,11 @@ function Footer() {
     <footer >
         <div className="container center">
           <div className="row">
-          <a className="grey-text" href="https://docs.google.com/document/d/1ozBWzyouy6FgoyrDW90FbzAmbWb-rNfD70IWsBV85-M/edit?usp=sharing">
-            <i class="far fa-file"></i>
+          <a className="grey-text" href="https://docs.google.com/document/d/16ADcaKgLQvL9U5Qq8-KJYXubDxbyXjrrusFbPDU3tuE/edit?usp=sharing">
+            <i class="far fa-file" title="resume"></i>
               </a>
-          <a className="grey-text" href="https://github.com/davidmbedford"><i class="fab fa-github"></i></a>
-          <a className="grey-text" href="https://www.linkedin.com/in/davidmbedford/"><i class="fab fa-linkedin"></i></a>
+          <a className="grey-text" href="https://github.com/davidmbedford"><i class="fab fa-github" title="github"></i></a>
+          <a className="grey-text" href="https://www.linkedin.com/in/davidmbedford/"><i class="fab fa-linkedin" title="linkedin"></i></a>
         </div>
         </div>
     </footer>
